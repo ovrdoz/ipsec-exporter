40da38b9a75ae467d2b51f5f674c3309aaff07d9
Diego Maia <diego.maia@digibee.com.br>
2020-03-18 18:22:17 -0300

    change build file with pattern names

--- build.sh
+++ build.sh
@@ -2,16 +2,10 @@
 set -e
 rm -rf ./bin
 
-#GOOS="darwin" GOARCH="amd64" go build -o "bin/digibee_darwin_amd64" main.go
-#GOOS="windows" GOARCH="amd64" go build -o "bin/digibee_windows_amd64.exe" main.go
-#GOOS="windows" GOARCH="386" go build -o "bin/digibee_windows_386.exe" main.go
-#GOOS="linux" GOARCH="amd64" go build -o "bin/digibee_linux_amd64" main.go
-#GOOS="linux" GOARCH="386" go build -o "bin/digibee_linux_386" main.go
-
-GOOS="linux" GOARCH="amd64" go build -ldflags="-s -w" -o "bin/digibee_linux_amd64"
+GOOS="darwin" GOARCH="amd64" go build  -ldflags="-s -w" -o "bin/ipsec-exporter_darwin_amd64"
+GOOS="windows" GOARCH="amd64" go build  -ldflags="-s -w" -o "bin/ipsec-exporter_windows_amd64.exe"
+GOOS="windows" GOARCH="386" go build  -ldflags="-s -w" -o "bin/ipsec-exporter_windows_386.exe"
+GOOS="linux" GOARCH="amd64" go build  -ldflags="-s -w" -o "bin/ipsec-exporter_linux_amd64"
+GOOS="linux" GOARCH="386" go build  -ldflags="-s -w" -o "bin/ipsec-exporter_linux_386"
 
 chmod +x ./bin/*
-
-git add --all &&\
-git commit -m "add new binaries" &&\
-git push origin master
