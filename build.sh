27a0b79ae5df7e498bbe39cc72368edd36e052b6
Diego Maia <diego.maia@digibee.com.br>
2020-03-18 18:56:31 -0300

    change build method and correct binaries

--- build.sh
+++ build.sh
@@ -2,10 +2,10 @@
 set -e
 rm -rf ./bin
 
-GOOS="darwin" GOARCH="amd64" go build  -ldflags="-s -w" -o "bin/ipsec-exporter_darwin_amd64"
-GOOS="windows" GOARCH="amd64" go build  -ldflags="-s -w" -o "bin/ipsec-exporter_windows_amd64.exe"
-GOOS="windows" GOARCH="386" go build  -ldflags="-s -w" -o "bin/ipsec-exporter_windows_386.exe"
-GOOS="linux" GOARCH="amd64" go build  -ldflags="-s -w" -o "bin/ipsec-exporter_linux_amd64"
-GOOS="linux" GOARCH="386" go build  -ldflags="-s -w" -o "bin/ipsec-exporter_linux_386"
+GOOS="darwin" GOARCH="amd64" go build -ldflags="-s -w" -o "bin/ipsec-exporter_darwin_amd64"
+GOOS="windows" GOARCH="amd64" go build -ldflags="-s -w" -o "bin/ipsec-exporter_windows_amd64.exe"
+GOOS="windows" GOARCH="386" go build -ldflags="-s -w" -o "bin/ipsec-exporter_windows_386.exe"
+GOOS="linux" GOARCH="386" go build -ldflags="-s -w" -o "bin/ipsec-exporter_linux_386"
+GOOS="linux" GOARCH="amd64" go build -ldflags="-s -w" -o "bin/ipsec-exporter_linux_amd64"
 
 chmod +x ./bin/*
